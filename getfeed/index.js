27a7bfcf7468a49547891de87360d89b0143627c
aaron <aaron@blinkmobile.com.au>
2017-09-30 13:51:03 +1000

    initial commit

---
+++ getfeed/index.js
@@ -0,0 +1,9 @@
+'use strict'
+module.exports = function (request) {
+  var parser = require('rss-parser')
+
+return "blah"//parser.parseURL('http://blinkmobile.com.au/BlinkMobileDeveloperBlogRSSFeed?format=feed', function(err, parsed) {
+		//"blah" + JSON.stringify(parsed.feed)
+		
+	//})
+}
